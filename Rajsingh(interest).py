ae526d51f9f035feb24ed42691d3c1c9de41a5d3
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-06 20:34:31 +0530

    Add files via upload

---
+++ Rajsingh(interest).py
@@ -0,0 +1,8 @@
+#program for calculate simple interest
+p=int(input())#varible for store principal
+r=int(input())#varible for store Rate
+t=int(input())#varible for store time
+i=float(p*r*t/100)
+print(i)
+
+
---
+++ rajsingh.py
@@ -0,0 +1,10 @@
+# This program adds two numbers
+
+num1 = 9.6 #no. 1 to be added
+num2 = 5.7 #no. 2 to be added
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
