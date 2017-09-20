b69d4067cf75ed19daa27b11bc9abe244054427d
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-20 23:17:59 +0530

    Add files via upload

---
+++ 20170920 print all keys of a dictionary.py
@@ -0,0 +1,11 @@
+#program for print all keys of a dictionary
+webster = {
+  "Aardvark" : "A star of a popular children's cartoon show.",
+  "Baa" : "The sound a goat makes.",
+  "Carpet": "Goes on the floor.",
+  "Dab": "A small amount."
+}
+
+for key in webster:
+  print webster[key]
+# Add your code below!
---
+++ 20170920 print dictionary items and key.py
@@ -0,0 +1,8 @@
+prices = {"banana": 4,"apple": 2,"orange": 1.5,"pear": 3}
+
+stock = {"banana": 6, "apple": 0, "orange": 32, "pear": 15}
+
+for raj in prices:
+  print raj
+  print "price: %s" % prices[raj]
+  print "stock: %s" % stock[raj]
