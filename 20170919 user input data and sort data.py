9423be5e8c3462782e884aa40eb5dea82b4b2f78
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-22 16:58:27 +0530

    Delete 20170919 user input data and sort data.py

--- 20170919 user input data and sort data.py
+++
@@ -1,8 +0,0 @@
-s=[]
-while True:
-    s = raw_input("Enter strings:")
-    if s:
-        words = [word for word in s.split(" ")]
-    else:
-        break;
-print " ".join(sorted(list(set(words))))
