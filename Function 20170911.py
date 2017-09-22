dc8048fb08af63bad13a1a2f1c30e433f93877d6
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-22 16:58:39 +0530

    Delete Function 20170911.py

--- Function 20170911.py
+++
@@ -1,9 +0,0 @@
-# Function decalare and calling functions
-def printme( str ):
-   "This prints a passed string into this function"
-   print str
-   return;
-
-# Now you can call printme function
-printme("Call function!")
-printme("Call again function!!")
