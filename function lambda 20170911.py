9f30e8a02679f695f7f26ff12cac774ad2e79a1b
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-22 16:59:52 +0530

    Delete function lambda 20170911.py

--- function lambda 20170911.py
+++
@@ -1,6 +0,0 @@
-# Function definition is here
-sum = lambda val1, val2: val1 + val2;
-
-# Now you can call sum as a function
-print "Value of total : ", sum(23, 87)
-print "Value of total : ", sum(29, 34)
