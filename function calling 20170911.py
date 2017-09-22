9c77a6e6b2fe50fb0f8001c1b9f5da7a41e03b3e
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-22 16:59:43 +0530

    Delete function calling 20170911.py

--- function calling 20170911.py
+++
@@ -1,8 +0,0 @@
-# Function definition is here
-def printprint(str):
-   "This prints a passed string into this function"
-   print str
-   return;
-
-# Now you can call printme function
-printprint ("Hey! my function is working")
