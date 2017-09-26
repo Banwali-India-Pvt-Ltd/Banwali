a45ad0555694c23761a243e4c5efb46db20bcc68
Banwali-India-Pvt-Ltd <30862678+Banwali-India-Pvt-Ltd@users.noreply.github.com>
2017-09-26 21:55:08 +0530

    Add files via upload

---
+++ 20170926 Python builtin mathematical function.py
@@ -0,0 +1,28 @@
+import math
+
+x=0.5 #x -- This must be a numeric value in the range -1 to 1. If x is greater than 1 then it will generate an error.
+print math.acos(x)#The method acos() returns the arc cosine of x, in radians.
+
+print math.asin(x)#The method asin() returns the arc sine of x, in radians.
+
+print math.atan(x)#Return the arc tangent of x, in radians.
+
+y = 0.50
+print math.atan2(y, x)#Return atan(y / x), in radians.
+
+print math.cos(x)#Return the cosine of x radians.
+
+print math.hypot(x, y)#Return the Euclidean norm, sqrt(x*x + y*y).
+
+print math.sin(x)#Return the sine of x radians.
+
+print math.tan(x)#Return the tangent of x radians.'''
+
+a=90
+print math.degrees(a)#Converts angle x from radians to degrees.
+
+print math.radians(a)#Converts angle x from degrees to radians.
+
+print math.pi #The mathematical constant pi.
+
+print math.e #The mathematical constant e.'''
